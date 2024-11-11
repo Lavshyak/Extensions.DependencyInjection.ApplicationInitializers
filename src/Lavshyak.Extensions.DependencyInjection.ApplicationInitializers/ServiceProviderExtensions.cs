54adb883525f16a8173cf779bcc87d8423a58840
Lavshyak <lavshyak@gmail.com>
2024-11-11 13:52:57 +0500

    improvements, InjectServicesAndInvokeInNewScopeAsync

--- src/Lavshyak.Extensions.DependencyInjection.ApplicationInitializers/ServiceProviderExtensions.cs
+++ src/Lavshyak.Extensions.DependencyInjection.ApplicationInitializers/ServiceProviderExtensions.cs
@@ -18,11 +18,12 @@ public static class ServiceProviderExtensions
             
             mark.MarkInitializationStarted();
 
-            using var scope = services.CreateScope();
+            using var initializersScope = services.CreateScope();
 
-            var initializers = scope.ServiceProvider.GetServices<IApplicationInitializer>();
+            var initializers = initializersScope.ServiceProvider.GetServices<IApplicationInitializer>();
             foreach (var initializer in initializers)
             {
+                using var initializerScope = initializersScope.ServiceProvider.CreateScope();
                 await initializer.ExecuteAsync();
             }
         }
@@ -30,5 +31,37 @@ public static class ServiceProviderExtensions
         {
             Semaphore.Release();
         }
+    }
+
+    public static async Task InjectServicesAndInvokeInNewScopeAsync<T1>(this IServiceProvider services,
+        Func<T1, Task> func) where T1 : notnull
+    {
+        await using var scope = services.CreateAsyncScope();
+
+        var t1 = scope.ServiceProvider.GetRequiredService<T1>();
+
+        await func(t1);
+    }
+
+    public static async Task InjectServicesAndInvokeInNewScopeAsync<T1, T2>(this IServiceProvider services,
+        Func<T1, T2, Task> func) where T1 : notnull where T2 : notnull
+    {
+        await using var scope = services.CreateAsyncScope();
+
+        var t1 = scope.ServiceProvider.GetRequiredService<T1>();
+        var t2 = scope.ServiceProvider.GetRequiredService<T2>();
+
+        await func(t1, t2);
+    }
+
+    public static async Task InjectServicesAndInvokeInNewScopeAsync<T1, T2, T3>(this IServiceProvider services,
+        Func<T1, T2, T3, Task> func) where T1 : notnull where T2 : notnull where T3 : notnull
+    {
+        await using var scope = services.CreateAsyncScope();
+
+        var t1 = scope.ServiceProvider.GetRequiredService<T1>();
+        var t2 = scope.ServiceProvider.GetRequiredService<T2>();
+        var t3 = scope.ServiceProvider.GetRequiredService<T3>();
+        await func(t1, t2, t3);
     }
 }
